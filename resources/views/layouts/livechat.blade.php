47dd8450fdf92266beb19f29dd8307b978485eda
ewehewa <ewehewa@gmail.com>
2026-08-28 17:52:06 +0100

    feat: add livechat

--- resources/views/layouts/livechat.blade.php
+++ resources/views/layouts/livechat.blade.php
@@ -24,6 +24,20 @@
 
 {{-- Paste Your  Live chart code  From Down Here --}}
 
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'faadd729ba548f8949496a8e9820b24713f04e0f';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
+
 
 
 
